e66accc12ccd3c7109139e3fc5fd184813428349
Uğur ALTUN <aziz_altun93@hotmail.com>
2025-06-13 01:09:13 +0300

    edit api.js

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,6 +1,12 @@
 const isDev = import.meta.env.DEV;
-const API_BASE_URL = isDev 
-  ? '/api' 
-  : `${import.meta.env.VITE_API_URL}/api`;
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+
+let API_BASE_URL;
+
+if (isDev) {
+  API_BASE_URL = '/api';
+} else {
+  API_BASE_URL = VITE_API_URL ? `${VITE_API_URL}/api` : 'https://crud-api.altuntech.com/api';
+}
 
 export default API_BASE_URL;
