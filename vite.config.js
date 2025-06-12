6124ad34d171fd71927b70d5975bea22e99cd7b7
Uğur ALTUN <aziz_altun93@hotmail.com>
2025-06-13 00:55:15 +0300

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:8000",
-        changeOrigin: true,
-        secure: true,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      },
-    },
-  },
 });
