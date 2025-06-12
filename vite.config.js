6ba0f010c09cf8b8922d3c9578e5c92c308c4703
Uğur ALTUN <aziz_altun93@hotmail.com>
2025-06-12 21:32:27 +0300

    add .env

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: process.env.VITE_API_URL || "http://localhost:8000",
         changeOrigin: true,
+        secure: true,
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
